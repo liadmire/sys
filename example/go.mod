034ff7358490967607ef076f0ea29042fe3e2a2f
rex <rex@123>
2020-05-31 12:23:12 +0800

    liadmire:   - sys     add example

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module sys
+
+go 1.14
+
+require github.com/liadmire/sys v0.0.0-20200530135126-a6327d6a6c63
---
+++ example/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/liadmire/sys"
+)
+
+func main() {
+	fmt.Println(sys.SelfDir(), sys.SelfPath(), sys.SelfName())
+}
