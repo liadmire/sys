1dd6f8268ac519df887f94155d1a4cfd098d607e
rexhu2020 <62680884+rexhu2020@users.noreply.github.com>
2020-10-10 17:12:56 +0800

    ip string to int, int ip to string

--- net.go
+++ net.go
@@ -1,1 +1,20 @@
 package sys
+
+import (
+	"fmt"
+	"math/big"
+	"net"
+)
+
+// StringIPToInt x
+func StringIPToInt(ipaddr string) int64 {
+	ret := big.NewInt(0)
+	ret.SetBytes(net.ParseIP(ipaddr).To4())
+	return ret.Int64()
+}
+
+// IntIPToString x
+func IntIPToString(ipaddr int64) string {
+	return fmt.Sprintf("%d.%d.%d.%d",
+		byte(ipaddr>>24), byte(ipaddr>>16), byte(ipaddr>>8), byte(ipaddr))
+}
